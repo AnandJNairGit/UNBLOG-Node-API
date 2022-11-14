f57ff49c5af0402230512e03d1f3c1597baf6822
AnandJNairGit <anandjnairit@gmail.com>
2022-11-14 23:17:42 +0530

    creatre server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//CONNECT MONGO DB
+// const mongooseConnectUrl = "";
+// mongoose.connect(mongooseConnectUrl);
+
+//error handling
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: { message: error.message } });
+});
+
+module.exports = app;
