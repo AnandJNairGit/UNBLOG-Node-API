5754f862fc96611fb77774e70bdf840609142ca8
AnandJNairGit <anandjnairit@gmail.com>
2022-11-16 20:16:47 +0530

    code refractor- move signup function to saperate function

--- API/routes/users/index.js
+++ API/routes/users/index.js
@@ -1,49 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { default: mongoose } = require("mongoose");
-const Users = require("../../models/users");
+//USERS MIDDLEWARES
+const signup = require("./signUp");
 
-const bcrypt = require("bcrypt");
+router.post("/signup", signup);
 
-router.post("/signup", async (req, res, next) => {
-    //GET USER BY EMAIL ID
-    const foundUser = await Users.find({ email: req.body.email });
-    console.log("the found user is------->>", foundUser);
-    //VALIDATE IF USER EXISTS OR NOT
-    if (foundUser.length >= 1) {
-      res.status(400).json({ message: "user email already exists" });
-    } else {
-      //HASH THE PASSWORD
-      bcrypt.hash(req.body.password, 10, async (err, hash) => {
-        if (err) {
-          res.status(500).json(err);
-        } else {
-          const user = new Users({
-            _id: new mongoose.Types.ObjectId(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hash,
-          });
-  
-          //   console.log("the user data is:------>", user);
-          try {
-            const userRes = await user.save();
-            console.log("the response is------>", userRes);
-            res.status(200).json({
-              message: "User Created",
-              userData: {
-                _id: userRes._id,
-                name: userRes.name,
-                email: userRes.email,
-              },
-            });
-          } catch (error) {
-            res.status(500).json(err);
-          }
-        }
-      });
-    }
-  });
-
-  module.exports = router;
+module.exports = router;
